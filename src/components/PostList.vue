d13c36e1e708cfe382bff4733bea329f1bccc5bd
DatBrian <vikard.2005@hotmail.com>
2023-06-22 00:47:40 -0500

    feat: :tada: The project was added to the repository
    The project was added to the repository with a basic functionality for API consumption.

---
+++ src/components/PostList.vue
@@ -0,0 +1,35 @@
+<template>
+    <h1>Listado de posts</h1>
+
+    <ul class="post-list">
+        <li v-for="post in posts" v-bind:key="post.id">{{ post.title }}</li>
+    </ul>
+</template>
+
+<script lang="ts" setup>
+import PostService from '@/services/PostServices';
+import { onMounted } from 'vue';
+
+const service = new PostService();
+const posts = service.getPosts();
+
+onMounted(async () => {
+    await service.fetchAll();
+})
+
+</script>
+
+<style scoped>
+.post-list {
+    width: 95vh;
+    height: 8rem;
+    padding: 1.3rem;
+    list-style-type: none;
+}
+
+.post-list{
+    padding: .8rem;
+    width: 100%;
+    border: 1px solid #ccc;
+}
+</style>
